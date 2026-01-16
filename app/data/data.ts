49f5bf4830842bd96128e17ac48e57082e2e5543
Steviggio <steve.mothmora@gmail.com>
2026-01-16 19:23:25 +0100

    fix(resources): enabling data's folder for push

---
+++ app/data/data.ts
@@ -0,0 +1,57 @@
+import { Github, Linkedin } from "./svgs";
+
+export const datas = {
+  name: "Steviggio",
+  title: "Développeur Fullstack React / Node.js",
+  about:
+    "Développeur React / Node.js, passionné de langues et de développement, j'assoc",
+  projects: [
+    {
+      title: "Polypost",
+      description: "Tableau de bord temps réel avec Next.js 14 et Tremor.",
+      link: "https://github.com",
+    },
+    {
+      title: "E-commerce Headless ",
+      description:
+        "Template E-commerce 'Headless' optimisé pour la performance.",
+      link: "https://github.com",
+    },
+    {
+      title: "Open Source CLI",
+      description: "Outil en ligne de commande pour l'automatisation de CI/CD.",
+      link: "https://github.com",
+    },
+    {
+      title: "Portfolio Minimaliste",
+      description: "Le site sur lequel vous naviguez actuellement.",
+      link: "https://github.com",
+    },
+  ],
+  experiences: [
+    {
+      company: "Efficience Digitale",
+      role: "Développeur Frontend",
+      date: "Sept. 2024 — Oct. 2025",
+    },
+    {
+      company: "Ganatan",
+      role: "Développeur Fullstack",
+      date: "Août 2022 - Sept. 2023",
+    },
+    {
+      company: "Freelance",
+      role: "Développeur Web",
+      date: "Août 2023 - Présent",
+    },
+  ],
+  socials: [
+    { name: "GitHub", url: "https://github.com/Steviggio", icon: Github },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/stevemothmora/",
+      icon: Linkedin,
+    },
+  ],
+  resumeUrl: "/Mothmora_2026.pdf",
+};
